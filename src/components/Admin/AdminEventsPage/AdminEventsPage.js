aa013f73f3d7ff9b68e17e80e78cd476b83ecfb0
Amie Thao <amie.c.thao@gmail.com>
2019-02-05 10:55:18 -0600

    add route for admin to view events

---
+++ src/components/Admin/AdminEventsPage/AdminEventsPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import connect from 'react-redux';
+
+
+
+
+class AdminEventsPage extends Component {
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default connect()(AdminEventsPage);

90175f91954867413690a920d7e88f7fa0b12ac2
Said Omar <saidomar@Saids-MacBook-Pro.local>
2019-02-05 14:59:40 -0600

    completed form

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const userRouter = require('./routes/user.router');
 const myEventsRouter = require('./routes/myEvents.router')
 const eventListRouter = require('./routes/eventList.router');
 const adminRouter = require('./routes/admin.router');
+const captainRouter = require('./routes/captain.router')
 
 
 // Body parser middleware
@@ -30,6 +31,7 @@ app.use('/api/user', userRouter);
 app.use('/api/myEvents', myEventsRouter);
 app.use('/api/eventList', eventListRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/captain' , captainRouter)
 
 // Serve static files
 app.use(express.static('build'));

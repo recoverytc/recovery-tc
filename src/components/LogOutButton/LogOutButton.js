538a5d2a0971cde88a36f148819f7a31b29de768
buckhalton <isaiah.buckhalton@gmail.com>
2019-02-07 11:56:16 -0600

    added logo to navbar, added additional navbar stlying.

--- src/components/LogOutButton/LogOutButton.js
+++ src/components/LogOutButton/LogOutButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import './LogOutButton.css';
 
 const LogOutButton = props => (
  
@@ -9,7 +9,7 @@ const LogOutButton = props => (
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
-      className={props.className}
+      className="button"
       onClick={() => props.dispatch({ type: 'LOGOUT' })}
     >
       Log Out
